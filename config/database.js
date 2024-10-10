9de37da5832a4fe19a492968c9f86cb8b6631b38
Anant Sharan Pandey <infiniteasp8>
2024-10-10 09:44:58 +0530

    add MONGODB_URI for deploymeny

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const uri = process.env.MONGODBURI;
+const uri = process.env.MONGODB_URI;
 const connectDB = async () => {
   try {
     await mongoose.connect(uri);
